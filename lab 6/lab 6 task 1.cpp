f5240e957764fe76892ae0a98ceaa3be92d7e4be
engmuhammadiqbalkhan-commits <engmuhammadiqbalkhan@gmail.com>
2026-04-22 03:11:25 -0700

    Add files via upload

---
+++ lab 6/lab 6 task 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>   // Library for input/output
+using namespace std;  // To use cout and cin without std::
+
+int main() {
+
+    int i = 1;  // Initialize variable starting from 1
+
+    // Loop runs while condition is true
+    while(i <= 10) {
+        cout << i << endl;  // Print current value of i
+        i++;                // Increase value of i by 1
+    }
+
+    return 0;  // End of program
+}
