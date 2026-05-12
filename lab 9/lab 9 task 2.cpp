68c9a242b9bb10add3fa8479f1af4c9d1f01ec54
engmuhammadiqbalkhan-commits <engmuhammadiqbalkhan@gmail.com>
2026-05-12 04:48:14 -0700

    Add files via upload

---
+++ lab 9/lab 9 task 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>      // Input/output stream library
+using namespace std;     // Use standard namespace
+
+// Function declaration
+void sayHello();
+
+int main() {
+    sayHello();          // Function call
+    return 0;            // End of program
+}
+
+// Function definition
+void sayHello() {
+    // Display message on screen
+    cout << "Hello, World!" << endl;
+}
