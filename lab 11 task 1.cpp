190ca1294125371fc1c36b527b1311ceafd49d34
engmuhammadiqbalkhan-commits <engmuhammadiqbalkhan@gmail.com>
2026-06-02 07:11:48 -0700

    Add files via upload

---
+++ lab 11 task 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string message = "Smart Traffic Control System";
+
+    cout << "Message: " << message << endl;
+
+    // Calculate string length
+    cout << "Length of String: " << message.length() << endl;
+
+    return 0;
+}
