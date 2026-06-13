db09a91200b6a33dfc89f90f2c2fd793b839d2a6
engmuhammadiqbalkhan-commits <engmuhammadiqbalkhan@gmail.com>
2026-06-13 08:40:41 -0700

    Add files via upload

---
+++ lab 11/tasks/lab 11 task 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string firstName = "Ali";
+    string lastName = "Khan";
+
+    // Concatenate strings
+    string fullName = firstName + " " + lastName;
+
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
